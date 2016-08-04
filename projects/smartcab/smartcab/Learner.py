6e73afde55acff455f731e00377120c85c93153a
unknown <ramy.zulficar@rzulficar7x64.cbmain.clarabridge.com>
2016-08-03 22:44:38 -0400

    adding agent to Learner

--- projects/smartcab/smartcab/Learner.py
+++ projects/smartcab/smartcab/Learner.py
@@ -1,8 +1,8 @@
 __author__ = 'ramy93'
 class Learner:
-    def __init__(self, environment):
+    def __init__(self, environment, agent):
         self.environment = environment
-
+        self.agent = agent
 
     def update(self,t):
         pass
