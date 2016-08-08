d0eb1a4cafa9552e4cedec1821d4302f2c630cac
unknown <ramy.zulficar@rzulficar7x64.cbmain.clarabridge.com>
2016-08-08 00:01:33 -0400

    Injecting planner to QlearningAgent

--- projects/smartcab/smartcab/QLearningAgent.py
+++ projects/smartcab/smartcab/QLearningAgent.py
@@ -10,16 +10,19 @@ from simulator import Simulator
 
 #going to inject a learner into
 import Learner
+from Learner import *
 
 class QLearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-    def __init__(self, env, Learner):
+    def __init__(self, env, learner, planner):
         super(QLearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+
+        self.planner = planner(self.env, self)
+        #self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-        self.learner = Learner  # this will be reference to injected learner
+        self.learner = learner  # this will be reference to injected learner
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -30,16 +33,16 @@ class QLearningAgent(Agent):
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
         # TODO: Update state
-        self.learner.update()
+        self.learner.update(self)
         # TODO: Select action according to your policy
-        action = None
+        action = self.next_waypoint
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
-
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        print "Updating Learning agent: "
+        print "deadline: {}\ninputs: {}\naction : {}\nreward: {}\ntime: {}\n".format(deadline, inputs, action, reward, self.env.t)
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
