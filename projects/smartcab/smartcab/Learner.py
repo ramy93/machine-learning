f3ad1fcdd78fde0efb46d553e13fddba32efc1eb
unknown <ramy.zulficar@rzulficar7x64.cbmain.clarabridge.com>
2016-08-07 23:53:51 -0400

    injecting planner into learner

--- projects/smartcab/smartcab/Learner.py
+++ projects/smartcab/smartcab/Learner.py
@@ -1,8 +1,18 @@
 __author__ = 'ramy93'
+
+
 class Learner:
-    def __init__(self, environment, agent):
+
+    def __init__(self, environment):
         self.environment = environment
-        self.agent = agent
-        self.Qtable =
-    def update(self,t):
-        pass
+        self.Qtable = None
+
+    def update(self, agent):
+        print 'Updating environment'
+        agent_next_waypoint = agent.get_next_waypoint()
+        agent_state = agent.get_state()
+        print 'agent state'
+        print agent_state
+
+
+
