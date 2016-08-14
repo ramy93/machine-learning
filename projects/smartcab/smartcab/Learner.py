5435fd249aaf7b0e4001d110c8c2f2d0c558ce3d
unknown <ramy.zulficar@rzulficar7x64.cbmain.clarabridge.com>
2016-08-14 18:39:46 -0400

    Modifying the get and set values for actions to send in dict format.

--- projects/smartcab/smartcab/Learner.py
+++ projects/smartcab/smartcab/Learner.py
@@ -5,9 +5,12 @@ from ctypes import *
 
 class Learner:
 
-    def __init__(self, environment):
+    def __init__(self, environment, state_dictionary=None, action_dictionary=None, alpha=0.2, gamma=0.9):
         self.environment = environment
-
+        self.state_dictionary = state_dictionary
+        self.action_dictionary = action_dictionary
+        self.alpha = alpha
+        self.gamma = gamma
     #method to be overriden by any learner class
     def update(self, state, action, reward, state_prime):
         print 'Updating environment'
@@ -19,11 +22,12 @@ class Learner:
         actions = {'left': 100, 'right': 2, 'forward': 100, None: 2}
         return actions[action]
 
-
+    def get_states(self):
+        return self.state_dictionary
 
 class QTableLearner(Learner):
-    def __init__(self, env, state_dictionary=None, action_dictionary=None, alpha=0.1, gamma=0.8):
-        Learner.__init__(self, env)
+    def __init__(self, env, state_dictionary=None, action_dictionary=None, alpha=0.2, gamma=0.8):
+        Learner.__init__(self, env, state_dictionary, action_dictionary, alpha, gamma)
         if state_dictionary is None or action_dictionary is None:
             raise Exception("State dictionary and action array cannot be null")
 
@@ -45,10 +49,18 @@ class QTableLearner(Learner):
     def update(self, state, action, reward, state_prime):
 
         currentQ = self.get_value(state, action)
+        #print
+        #print currentQ, 'Current Q value'
         new_value = (1-self.alpha) * currentQ + self.alpha * (reward + self.gamma * max([ self.get_value(
-            state_prime, {'action': self.environment.valid_actions}) ]))
+            state_prime,  action_prime) for action_prime in self.environment.valid_actions ]))
 
+        #print new_value, 'updated Q value'
         self.set_value(state,action,new_value)
 
+        #print self.get_value(state, action)
+        #print self.table.table
+        return
     def set_value(self, state, action, value):
+        action = {'action': action}
         self.table.set_value(state, action, value)
+
